dfdfb3cc5f68c657ea018512572d69d1856e5ffb
Donal Burke <dbrke98@gmail.com>
2018-11-26 16:19:36 +0000

    Changed code layout and added main function, need to add command line interface for user interaction

--- digitrec.py
+++ digitrec.py
@@ -16,7 +16,19 @@ def load_data():
     # Load dataset from keras 
     return mnist.load_data()
 
-def build_model():
+def build_model(X_train, y_train, X_test, y_test):
+    # reshape to be samples, pixels, width, height
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+    # normalize inputs from 0-255 to 0-1
+    X_train = X_train / 255
+    X_test = X_test / 255
+    # one hot encode outputs
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
+    num_classes = y_test.shape[1]
+
     model = Sequential()
     model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -24,29 +36,21 @@ def build_model():
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # Fit the model
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
     return model
 
-(X_train, y_train), (X_test, y_test) = load_data()
-# reshape to be samples, pixels, width, height
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+def main():
+    (X_train, y_train), (X_test, y_test) = load_data()
 
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+    model = build_model(X_train, y_train, X_test, y_test)
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("CNN Error: %.2f%%" % (100-scores[1]*100))
 
-model = build_model()
-# Compile model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.save('digitrec_model.h5')
 
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("CNN Error: %.2f%%" % (100-scores[1]*100))
-
-model.save('digitrec_model.h5')
+if __name__ == '__main__':
+   main()
